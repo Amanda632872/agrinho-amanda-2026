97413275fdd595f8164cdf158dd2efbdc2d2528e
Amanda632872 <l.amanda19@escola.pr.gov.br>
2026-06-10 14:04:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Aguarda o carregamento completo da página
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("🌱 Projeto Agrinho 2026 carregado com sucesso!");
+    
+    // Você pode adicionar funções interativas aqui no futuro!
+});
